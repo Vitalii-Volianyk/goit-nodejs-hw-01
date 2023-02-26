6ee5ba055ba6067efd62780d4d09ea89565b3d3a
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-02-26 09:24:21 +0200

    modified:   db/contacts.json 	modified:   index.js

--- index.js
+++ index.js
@@ -21,21 +21,27 @@ const argv = program.opts();
 function invokeAction({ action, id, name, email, phone }) {
 	switch (action) {
 		case "list":
-			listContacts().then(data => console.table(data));
+			listContacts()
+				.then(data => console.table(data))
+				.catch(err => console.log(err));
 			break;
 
 		case "get":
-			getContactById(id).then(data => console.table(data));
+			getContactById(id)
+				.then(data => console.table(data))
+				.catch(err => console.log(err));
 			break;
 
 		case "add":
-			addContact({ name, email, phone }).then(data =>
-				console.table(data)
-			);
+			addContact({ name, email, phone })
+				.then(data => console.table(data))
+				.catch(err => console.log(err));
 			break;
 
 		case "remove":
-			removeContact(id).then(data => console.table(data));
+			removeContact(id)
+				.then(data => console.table(data))
+				.catch(err => console.log(err));
 			break;
 
 		default:
